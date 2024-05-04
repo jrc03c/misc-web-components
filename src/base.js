be67f6aafa9101ccca41f47885133324f727ff0d
jrc03c <jrc03c@pm.me>
2024-05-04 09:17:30 -0500

    ...

--- src/base.js
+++ src/base.js
@@ -1,140 +1,29 @@
-const makeKey = require("@jrc03c/make-key")
-
 class BaseComponent extends HTMLElement {
-  static css = ``
-  static instanceCount = 0
-  static styleElement = null
-  static template = "<slot></slot>"
-
-  mutationObservers = []
+  static template = `
+    <template>
+      <div class="x-base">
+        <slot></slot>
+      </div>
+    </template>
+  `
 
   constructor() {
     super()
 
-    this.id = makeKey(8)
+    const parser = new DOMParser()
 
-    const currentHTML = this.innerHTML
-    this.innerHTML = this.constructor.template
+    const fragment = parser.parseFromString(
+      this.constructor.template,
+      "text/html",
+    )
 
-    if (currentHTML.length > 0) {
-      const currentHTMLContainsSlots = currentHTML.match(/<.*?slot=".*?".*?>/g)
-      const slots = Array.from(this.querySelectorAll("slot"))
+    const shadow = this.attachShadow({ mode: "open" })
 
-      if (slots.length > 0) {
-        if (currentHTMLContainsSlots) {
-          const parser = new DOMParser()
-          const fragment = parser.parseFromString(currentHTML, "text/html")
-
-          const children = Array.from(fragment.head.children).concat(
-            Array.from(fragment.body.children),
-          )
-
-          children.forEach(child => {
-            const slotName = child.getAttribute("slot") || "(none)"
-
-            const slot =
-              slotName === "(none)"
-                ? slots[0]
-                : slots.find(slot => slot.name === slotName)
-
-            if (slot) {
-              slot.parentElement.replaceChild(child, slot)
-            } else {
-              throw new Error(
-                `\`${this.constructor.name}\` elements do not contain "${slotName}" slots!`,
-              )
-            }
-          })
-        } else {
-          this.innerHTML = this.innerHTML.replace(
-            /<slot.*?>.*?<\/slot>/g,
-            currentHTML,
-          )
-        }
-      } else {
-        throw new Error(
-          `A \`${this.constructor.name}\` element received slotted content, but \`${this.constructor.name}\` elements have no slots in which to receive such content!`,
-        )
-      }
-    }
-  }
-
-  addEventListener() {
-    const remover = () => {
-      super.removeEventListener(...arguments)
-      super.removeEventListener("unmount", remover)
-    }
-
-    super.addEventListener("unmount", remover)
-    super.addEventListener(...arguments)
-    return remover
-  }
-
-  connectedCallback() {
-    this.constructor.instanceCount++
-
-    if (this.constructor.instanceCount === 1) {
-      this.constructor.styleElement = document.createElement("style")
-      this.constructor.styleElement.innerHTML = this.constructor.css
-      document.body.appendChild(this.constructor.styleElement)
-    }
-
-    const attributeObserver = new MutationObserver(mutations => {
-      for (const mutation of mutations) {
-        if (mutation.type === "attributes") {
-          this.dispatchEvent(
-            new CustomEvent("attribute-change:" + mutation.attributeName),
-          )
-        }
-      }
-    })
-
-    attributeObserver.observe(this, { attributes: true })
-    this.mutationObservers.push(attributeObserver)
-
-    this.dispatchEvent(new CustomEvent("mount"))
-    return this
-  }
-
-  disconnectedCallback() {
-    this.constructor.instanceCount--
-
-    if (this.constructor.instanceCount < 1) {
-      this.constructor.styleElement.parentElement.removeChild(
-        this.constructor.styleElement,
-      )
-    }
-
-    this.mutationObservers.forEach(observer => observer.disconnect())
-    this.dispatchEvent(new CustomEvent("unmount"))
-    return this
-  }
-
-  emit(eventName, payload) {
-    return this.dispatchEvent(new CustomEvent(eventName, { details: payload }))
-  }
-
-  off() {
-    return this.removeEventListener(...arguments)
-  }
-
-  offAttributeChange(attr, callback) {
-    return this.removeEventListener("attribute-change:" + attr, callback)
-  }
-
-  on() {
-    return this.addEventListener(...arguments)
-  }
-
-  onAttributeChange(attr, callback) {
-    return this.addEventListener("attribute-change:" + attr, callback)
-  }
-
-  toObject() {
-    return {
-      id: this.id,
-    }
+    shadow.appendChild(
+      fragment.querySelector("template").content.cloneNode(true),
+    )
   }
 }
 
+customElements.define("x-base", BaseComponent)
 module.exports = BaseComponent
