41bd433b26e6b9cb843b09bae37b033e573031dc
jrc03c <jrc03c@pm.me>
2024-05-04 09:46:45 -0500

    ...

--- src/base.js
+++ src/base.js
@@ -1,3 +1,40 @@
+const { getAllElements } = require("./utils")
+
+Object.defineProperty(Node.prototype, "localShadowRoot", {
+  configurable: true,
+  enumerable: true,
+
+  get() {
+    if (this instanceof ShadowRoot) {
+      return this
+    }
+
+    if (this.shadowRoot) {
+      return this.shadowRoot
+    }
+
+    let temp = this
+
+    while (temp.parentNode) {
+      if (temp.parentNode instanceof ShadowRoot) {
+        return temp.parentNode
+      }
+
+      if (temp.parentNode.shadowRoot) {
+        return temp.parentNode.shadowRoot
+      }
+
+      temp = temp.parentNode
+    }
+
+    return undefined
+  },
+
+  set() {
+    throw new Error("The `localShadowRoot` property is read-only!")
+  },
+})
+
 class BaseComponent extends HTMLElement {
   static template = `
     <template>
@@ -22,6 +59,14 @@ class BaseComponent extends HTMLElement {
     shadow.appendChild(
       fragment.querySelector("template").content.cloneNode(true),
     )
+  }
+
+  querySelector() {
+    return getAllElements(this).find(el => el.matches(...arguments))
+  }
+
+  querySelectorAll() {
+    return getAllElements(this).filter(el => el.matches(...arguments))
   }
 }
 
